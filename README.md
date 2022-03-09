c0c3a16635643df5add3944f8b8beb3e14a51dc4
Gerardo Steve Muñoz Contreras <3004636930101@ingenieria.usac.edu.gt>
2022-03-09 00:11:25 -0600

    Crearcion scrip de la base y llenado de la base falta revisar la tabla evalucacion

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BD1_Proyecto1_201900853
+Primer Semestre 2022 
+
+
+Gerardo Steve Muñoz Contreras 
--- temporal.sql
+++ temporal.sql
@@ -2,28 +2,31 @@
 -- consulta para eliminar tabla si esta existe
 
 DROP TABLE  temporal;
+TRUNCATE TABLE temporal;
 SELECT * FROM temporal;
 SELECT COUNT(*) FROM TEMPORAL;
+-- SELECT distinct nombre_empleado,apellido_empleado FROM temporal;
+
 create table temporal(
-                         nombre_empleado varchar(50),
-                         apellido_empleado varchar(50),
-                         direccion_empleado varchar(50),
-                         telefono_empleado varchar(50),
-                         genero_empleado varchar(50),
-                         fecha_nacimiento_empleado varchar(50),
-                         titulo_del_empleado varchar(50),
-                         nombre_paciente varchar(50),
-                         apellido_paciente varchar(50),
-                         direccion_paciente varchar(50),
-                         telefono_paciente varchar(50),
-                         genero_paciente varchar(50),
-                         fecha_nacimiento_paciente varchar(50),
-                         altura varchar(50),
-                         peso varchar(50),
-                         fecha_evaluacion varchar(50),
-                         sintoma_del_paciente varchar(50),
-                         diagnostico_del_sintoma varchar(50),
-                         rango_del_diagnostico varchar(50),
-                         fecha_tratamiento varchar(50),
-                         tratamiento_aplicado varchar(50)
+                         nombre_empleado varchar(300),
+                         apellido_empleado varchar(250),
+                         direccion_empleado varchar(250),
+                         telefono_empleado varchar(250),
+                         genero_empleado varchar(250),
+                         fecha_nacimiento_empleado date,
+                         titulo_del_empleado varchar(250),
+                         nombre_paciente varchar(250),
+                         apellido_paciente varchar(250),
+                         direccion_paciente varchar(250),
+                         telefono_paciente varchar(250),
+                         genero_paciente varchar(250),
+                         fecha_nacimiento_paciente date,
+                         altura number,
+                         peso number,
+                         fecha_evaluacion date,
+                         sintoma_del_paciente varchar(250),
+                         diagnostico_del_sintoma varchar(250),
+                         rango_del_diagnostico number,
+                         fecha_tratamiento date,
+                         tratamiento_aplicado varchar(250)
 );
